de2e3a77d90526c6e8cab1ea7550c066db252f28
Nishant Jha <me@nishantjha.org>
2021-02-22 22:01:31 -0500

    init

---
+++ viz/src/App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Viz } from "./components/Viz";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1> Fel d 1 Viz</h1>
+        <Viz />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ viz/src/components/Viz.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Viz = () => {
+  return (
+    <div>
+      <h3> Hello World </h3>
+    </div>
+  )
+}
